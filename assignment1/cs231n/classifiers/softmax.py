d301d93731ba11ba4ac27e8c165359e1eff6aee8
Pratyush Nath Sahu <f20180620@hyderabad.bits-pilani.ac.in>
2020-02-11 00:26:35 +0530

    Softmax Solved

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -32,9 +32,27 @@ def softmax_loss_naive(W, X, y, reg):
     # regularization!                                                           #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    scores = X.dot(W)
+    num_train = X.shape[0]
+    num_classes = W.shape[1]
+    
+    for i in range(num_train):
+        f = (scores[i]) - np.max(scores[i]) # avoid numerical instability
+        softmax = np.exp(f)/np.sum(np.exp(f))
+        loss += -np.log(softmax[y[i]])
+        # Weight Gradients
+        for j in range(num_classes):
+            dW[:,j] += X[i] * softmax[j]
+            dW[:,y[i]] -= X[i]
 
-    pass
+    # Average
+    loss /= num_train
+    dW /= num_train
 
+    # Regularization
+    loss += reg * np.sum(W * W)
+    dW += reg * 2 * W                              
+                               
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
     return loss, dW
@@ -57,8 +75,30 @@ def softmax_loss_vectorized(W, X, y, reg):
     # regularization!                                                           #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    num_train = X.shape[0]
+    scores = X.dot(W)
+    scores = scores - np.max(scores, axis=1, keepdims=True)
+  
+    # Softmax Loss
+    sum_exp_scores = np.exp(scores).sum(axis=1, keepdims=True)
+    softmax_matrix = np.exp(scores)/sum_exp_scores
+    loss = np.sum(-np.log(softmax_matrix[np.arange(num_train), y]) )
 
-    pass
+    # Weight Gradient
+    softmax_matrix[np.arange(num_train),y] -= 1
+    dW = X.T.dot(softmax_matrix)
+
+    # Average
+    loss /= num_train
+    dW /= num_train
+
+    # Regularization
+    loss += reg * np.sum(W * W)
+    dW += reg * 2 * W 
+
+    
+    
+    
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
